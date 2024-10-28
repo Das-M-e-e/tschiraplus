afea659e90df227e34eb090ebd119596134dbdf2
Das_M_e_e_ <151558417+Das-M-e-e@users.noreply.github.com>
2024-10-28 12:51:05 +0100

    Created Task model, TaskPriority enum & TaskStatus enum

---
+++ tschiraplus/Core/Enums/TaskStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace Core.Enums;
+
+public enum TaskStatus
+{
+    Backlog,
+    Ready,
+    InProgress,
+    InReview,
+    Done
+}
---
+++ tschiraplus/Core/Models/TaskModel.cs
@@ -0,0 +1,25 @@
+﻿using Core.Enums;
+using TaskStatus = Core.Enums.TaskStatus;
+
+namespace Core.Models;
+
+public class TaskModel
+{
+    private Guid Id { get; set; }
+    private string? Title { get; set; }
+    private string? Description { get; set; }
+    private TaskStatus? Status { get; set; }
+    private TaskPriority? Priority { get; set; }
+    private DateTime CreationDate { get; set; }
+    private DateTime? DueDate { get; set; }
+    private DateTime? CompletionDate { get; set; }
+    //private List<User>? Assignees { get; set; }
+    private List<string> Tags { get; set; } = [];
+    //private Guid? SprintId { get; set; }
+    //private Guid ProjectId { get; set; }
+    //private TimeSpan? EstimatedTime { get; set; }
+    //private TimeSpan? ActualTimeSpent { get; set; }
+    //private List<Attachment> Attachments { get; set; }
+    //private List<Comment> Comments { get; set; }
+    //private List<Guid> Dependencies { get; set; }
+}
