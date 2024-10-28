22f46e5e3fe2b6922a430a46c626c385ac5cc0f5
Newton <ik066@hdm-stuttgart.de>
2024-10-28 12:51:45 +0100

    Absolut primitives Gerüst der ersten Task Klasse. Es muss noch geklärt werden ob von ihr geerbt wird oder umgehkert. Werde heite Abend nachfragen.

---
+++ tschiraplus/Core/Task.cs
@@ -0,0 +1,23 @@
+﻿namespace Core;
+
+//Nur ein absolutes Skellet von einer Klasse werde später noch mehr daran arbeiten
+public class Task
+{
+    
+    String titel { get; set; }
+    String[] description { get; set; } //String Array um Zeilen-Umbrüche zu handeln
+    
+    /* Yet to implement
+     Date Begin {get; set; };
+     Date End
+      Or somthing like that
+    */
+
+    public task(String titel, String[] description)
+    {
+        this.titel = titel;
+        this.description = description;
+    }
+    
+    
+}
