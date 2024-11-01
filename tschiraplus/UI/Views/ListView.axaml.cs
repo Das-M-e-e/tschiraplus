f8385ce90bdd46b3ef1fe1f31631d13d9d90517b
Franzi <fl062@hdm-stuttgart.de>
2024-11-01 19:01:26 +0100

    Grobes Design von der List View

---
+++ tschiraplus/UI/Views/ListView.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace UI.Views;
+
+public partial class ListView : Window
+{
+    public ListView()
+    {
+        InitializeComponent();
+    }
+}
