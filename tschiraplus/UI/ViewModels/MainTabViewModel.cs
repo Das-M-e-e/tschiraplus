7965905e13fda0b8aabe7ca054f8838719ec2519
Das_M_e_e_ <151558417+Das-M-e-e@users.noreply.github.com>
2024-11-01 16:44:02 +0100

    Create main window tab view

---
+++ tschiraplus/UI/ViewModels/MainTabViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.ObjectModel;
+using Avalonia.Controls;
+using Avalonia.Controls.Chrome;
+
+namespace UI.ViewModels;
+
+public class MainTabViewModel : ViewModelBase
+{
+    public ObservableCollection<TabItemModel> Tabs { get; }
+
+    public MainTabViewModel()
+    {
+        Tabs = new ObservableCollection<TabItemModel>
+        {
+            new TabItemModel("One", new Window()),
+            new TabItemModel("Two", new Window())
+        };
+    }
+}
---
+++ tschiraplus/UI/Views/MainTabView.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace UI.Views;
+
+public partial class MainTabView : Window
+{
+    public MainTabView()
+    {
+        InitializeComponent();
+    }
+}
