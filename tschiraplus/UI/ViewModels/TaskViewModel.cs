5962354db5e578b62518c57af3ff69df88ac695e
SozukaLab <koumpatidou.sofia@gmail.com>
2024-11-05 23:45:42 +0100

    Finished up ScrollViewers of all Task Columns

--- tschiraplus/UI/ViewModels/TaskViewModel.cs
+++ tschiraplus/UI/ViewModels/TaskViewModel.cs
@@ -3,6 +3,6 @@ namespace UI.ViewModels;
 public class TaskViewModel
 {
     public string Title { get; set; }
-    public string Description { get; set; }
-    public string Status { get; set; }
+    public string Tag { get; set; }
+    public string User { get; set; }
 }
