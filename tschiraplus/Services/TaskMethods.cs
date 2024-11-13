0a0d18d401e105c108925fe02a4e34d9bf561467
Anna-Lena <ma157@hdm-stuttgart.de>
2024-11-13 11:08:26 +0100

    Erstellt: Grobe Idee für eine Create Methode

---
+++ tschiraplus/Services/TaskMethods.cs
@@ -0,0 +1,24 @@
+﻿
+namespace Services;
+using Core.Models;
+
+public class TaskMethods
+{
+    private List<TaskModel> taskList = new List<TaskModel>();
+    //private var TaskModels = taskList;
+
+    TaskModel CreateTask(Guid taskId, DateTime creationDate, List<string> tags, Guid projectId, List<AttachmentModel> attachments)
+    {
+        var newTask = new TaskModel
+        {
+            TaskId = taskId,
+            CreationDate = creationDate,
+            //Tags = tags,
+            ProjectId = projectId,
+            //Attachments = attachments
+        };
+        taskList.Add(newTask);    
+        return newTask;
+    }
+
+}
