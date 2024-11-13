3716ab334d04be9c3c746e2fb1565db4aa4f56f9
Newton <ik066@hdm-stuttgart.de>
2024-11-13 12:05:58 +0100

    CreateTaskModell als einen Konstruktor.

--- tschiraplus/Services/TaskMethods.cs
+++ tschiraplus/Services/TaskMethods.cs
@@ -1,4 +1,7 @@
 ﻿
+using Core.Enums;
+using TaskStatus = System.Threading.Tasks.TaskStatus;
+
 namespace Services;
 using Core.Models;
 
@@ -7,17 +10,45 @@ public class TaskMethods
     private List<TaskModel> taskList = new List<TaskModel>();
     //private var TaskModels = taskList;
 
-    TaskModel CreateTask(Guid taskId, DateTime creationDate, List<string> tags, Guid projectId, List<AttachmentModel> attachments)
+    TaskModel CreateTaskModell(Guid taskId
+                        ,string title
+                        ,string description
+                        ,TaskStatus status
+                        ,TaskPriority priority
+                        ,DateTime creationDate
+                        ,DateTime dueDate
+                        ,DateTime completionDate
+                        //,List<GuId> assignes
+                        //,List<Strings> tags
+                        ,Guid sprintId
+                        ,Guid projectId
+                        ,TimeSpan estimatedTime
+                        ,TimeSpan actualTimeSpend
+                        //,List<Guid> attachments
+                        //,List<Guid> comments
+                        //,List<Guid> dependencies
+                        )
     {
-        var newTask = new TaskModel
-        {
-            TaskId = taskId,
-            CreationDate = creationDate,
-            //Tags = tags,
-            ProjectId = projectId,
-            //Attachments = attachments
-        };
-        taskList.Add(newTask);    
+        TaskModel newTask = new TaskModel();
+        newTask.TaskId = taskId;
+        newTask.Title = title;
+        newTask.Description = description;
+        newTask.Status = status as Core.Enums.TaskStatus?;
+        newTask.Priority = priority;
+        newTask.CreationDate = creationDate;
+        newTask.DueDate = dueDate;
+        newTask.CompletionDate = completionDate;
+        //newTask.Assignes = assignes;
+        //newTask.Tags = tags;
+        newTask.SprintId = sprintId;
+        newTask.ProjectId = projectId;
+        newTask.EstimatedTime = estimatedTime;
+        newTask.ActualTimeSpent = actualTimeSpend;
+        //newTask.Attachments = attachments;
+        //newTask.Comments = comments;
+        //newTask.Dependencies = dependencies;
+        
+        
         return newTask;
     }
 
