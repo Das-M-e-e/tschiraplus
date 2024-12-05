dcc8e2f1c1c36c5a0cb07f22a4178a05af17c017
Newton <ik066@hdm-stuttgart.de>
2024-12-05 01:20:06 +0100

    #12 CreateTask Methode endlich erstellt. Es fehlen noch Bindings und es muss noch getestet werden.

--- tschiraplus/UI/ViewModels/TaskCreationViewModel.cs
+++ tschiraplus/UI/ViewModels/TaskCreationViewModel.cs
@@ -4,14 +4,25 @@ using System.Text.RegularExpressions;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 using Services.DTOs;
+using Services.Repositories;
+using Services.TaskServices;
 
 namespace UI.ViewModels;
 
 public class TaskCreationViewModel : ViewModelBase
 {
+    private readonly TaskRepository _taskRepository;
+    private readonly TaskService _taskService;
+
+    public TaskCreationViewModel(TaskRepository taskRepository, TaskService taskService) //Konstrukto (evntl. unötig?)
+    {
+        _taskRepository = taskRepository;
+        _taskService = taskService;
+    }
     
-    
-    private TaskDto CreateTask()
+    //Erstellt eine TaskDTo
+    //TODO Bindings zu den entsprechenden Attributen.
+    private TaskDto CreateTaskDTO()
     {
         TaskDto dto = new TaskDto()
         {
@@ -21,7 +32,16 @@ public class TaskCreationViewModel : ViewModelBase
             Status = string.Empty,
             CreationDate = DateTime.Today
         };
+        
+        
         return dto;
+    }
+
+    //Stellt TaskDTo in die Datenbank
+    private void CreateTask(TaskDto dto)
+    {
+        _taskRepository.AddTask(                            //Ruft db in Repo auf und speichert Task
+            _taskService.convertTaskDtoToTaskModel(dto));   //Wandelt dto zu TaskModell
     }
     
 }
