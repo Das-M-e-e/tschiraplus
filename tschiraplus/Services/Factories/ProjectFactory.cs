3a5a22a0caa093eb7be9f248eb0647585980bae2
Anna-Lena <ma157@hdm-stuttgart.de>
2025-01-10 20:32:41 +0100

    Erstellen der drei Factories ProjectFactory.cs, ProjectDTOFactory.cs, TaskDTOFactory.cs

---
+++ tschiraplus/Services/Factories/ProjectFactory.cs
@@ -0,0 +1,44 @@
+﻿using Core.Models;
+using Core.Enums;
+using Services.Repositories;
+
+public class ProjectFactory
+{
+    private readonly IProjectRepository _projectRepository;
+    private readonly ITaskRepository _taskRepository; //TODO nötig???
+    private readonly IUserRepository _userRepository;
+
+    public ProjectFactory(IProjectRepository projectRepository, ITaskRepository taskRepository, IUserRepository userRepository)
+    {
+        _projectRepository = projectRepository;
+        _taskRepository = taskRepository; // TODO nötig??
+        _userRepository = userRepository;
+    }
+
+    public async Task<ProjectModel> GetProjectFromDatabase(Guid projectId)
+    {
+        // Lädt das Projekt aus der Datenbank
+        var projectEntity = await _projectRepository.GetProjectByIdAsync(projectId);
+
+        // Lädt den Besitzer (UserModel) des Projekts
+        var owner = await _userRepository.GetUserByIdAsync(projectEntity.OwnerId);
+
+        
+
+        // Erstellet das ProjectModel
+        return new ProjectModel
+        {
+            ProjectId = projectEntity.ProjectId,
+            OwnerId = owner.UserId,
+            Name = projectEntity.Name,
+            Description = projectEntity.Description,
+            Status = projectEntity.Status,
+            Priority = projectEntity.Priority,
+            CreationDate = projectEntity.CreationDate,
+            StartDate = projectEntity.StartDate,
+            DueDate = projectEntity.DueDate,
+            LastUpdated = projectEntity.LastUpdated,
+            //Tasks = tasks
+        };
+    }
+}
---
+++ tschiraplus/Services/Factories/TaskDTOFactory.cs
@@ -0,0 +1,29 @@
+﻿using Services.DTOs;
+
+namespace Services.Factories;
+
+public class TaskDTOFactory
+{
+    public TaskDto CreateTaskDto(
+        Guid TaskId,
+        string Title,
+        string Description,
+        string Status,
+        DateTime CreationDate  
+        
+        )
+    {
+        var TaskDTO = new TaskDto
+        {
+            TaskId = TaskId,
+            Title = Title,
+            Description = Description,
+            Status = Status,
+            CreationDate = CreationDate
+        };
+        
+        
+      return  TaskDTO;
+    }
+    
+}
