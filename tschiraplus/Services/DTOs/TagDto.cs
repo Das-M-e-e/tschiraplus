50e3039bc4b42b223a01359430f3cb111fb831f9
Polina Rogge <pr077@hdm-stuttgart.de>
2025-01-13 18:34:32 +0100

    TagDto.cs erstellt und Atribute hinzugefügt

---
+++ tschiraplus/Services/DTOs/TagDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Services.DTOs;
+
+public class TagDto
+{
+    public Guid TagId { get; init; }
+    public string Title { get; init; }
+    public Guid ProjectId { get; set; }
+    public string? Description { get; set; }
+    public string ColorCode { get; set; }
+    
+}
