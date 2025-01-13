79fc81d656359b9c9c131dde8d0ed630fb41acf1
Anna-Lena <ma157@hdm-stuttgart.de>
2025-01-13 11:29:41 +0100

    Erstellen der Mapperklassen ProjectMapper.cs und TaskMapper.cs

---
+++ tschiraplus/Services/Mapper/ProjectMapper.cs
@@ -0,0 +1,44 @@
+﻿using Core.Models;
+using Services.DTOs;
+
+namespace Services.Mapper;
+
+public class ProjectMapper
+{
+    public static ProjectDto toDto(ProjectModel model)
+    {
+        var projectDto = new ProjectDto()
+        {
+            ProjectId = model.ProjectId,
+            Name = model.Name,
+            Description = model.Description,
+            ProjectPriority = model.Priority.ToString()
+        
+        };
+
+        return projectDto;
+
+    }
+
+    public static ProjectModel toModel(ProjectDto dto, UserModel user)
+    {
+        var ProjectModel = new ProjectModel()
+        {
+            ProjectId = dto.ProjectId,
+            OwnerId = user.UserId,
+            Name = dto.Name,
+            Description = dto.Description,
+            //TODO muss noch geparsed werden Status = dto.ProjectStatus,
+            CreationDate = DateTime.Now,
+            // TODO soll das noch in die UI eingearbeitet werden?? StartDate = 
+            // TODO DueDate noch in UI einarbeiten 
+            //TODO Lastupdated wo rausholen
+            
+        };
+        
+        return ProjectModel;
+    }
+
+
+
+}
---
+++ tschiraplus/Services/Mapper/TaskMapper.cs
@@ -0,0 +1,66 @@
+﻿using System.Data;
+using Core.Models;
+using Services.DTOs;
+
+namespace Services.Mapper;
+
+public class TaskMapper
+{
+    public static TaskDto ToDto(TaskModel model)
+    {
+        var taskDto = new TaskDto()
+        {
+          TaskId  = model.TaskId,
+          Title = model.Title,
+          Description = model.Description,
+          Status = model.Status.ToString(),
+          CreationDate = model.CreationDate
+        };
+
+        return taskDto;
+    }
+
+
+
+    public static TaskModel ToModel(TaskDto dto, UserModel owner)
+    {
+        var taskModel = new TaskModel()
+        {
+            TaskId = dto.TaskId,
+            Title = dto.Title,
+            //TODO get information from somewhere ProjectId = ,
+            //TODO get information from somewhere SprintID = ,
+            //TODO get information from somewhere AuthorID = ,
+            Description = dto.Description,
+            // DoDO parse Status = dto.Status ,
+            //TODO get information from somewhere Priority??? = ,
+            CreationDate = dto.CreationDate
+            //TODO get information from somewhere DueDate = ,
+            //TODO get information from somewhere CompletionDate = ,
+            //TODO get information from somewhere LastUpdated = ,
+            //TODO get information from somewhere EstimatedTime = ,
+            //TODO get information from somewhere ActualTimeSpent = ,
+            
+            
+        };
+        
+        return taskModel;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
