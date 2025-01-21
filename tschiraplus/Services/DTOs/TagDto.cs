6defe83a90a691c494b344037a311aa6622cf487
Polina Rogge <pr077@hdm-stuttgart.de>
2025-01-21 22:35:17 +0100

    In TagDto and TagModel ProjectId -> nullable deleted AuthorId and CreatedAt in TagModel

--- tschiraplus/Services/DTOs/TagDto.cs
+++ tschiraplus/Services/DTOs/TagDto.cs
@@ -4,7 +4,7 @@ public class TagDto
 {
     public Guid TagId { get; init; }
     public string Title { get; init; }
-    public Guid ProjectId { get; init; }
+    public Guid? ProjectId { get; init; }
     public string? Description { get; init; }
     public string ColorCode { get; init; }
     
