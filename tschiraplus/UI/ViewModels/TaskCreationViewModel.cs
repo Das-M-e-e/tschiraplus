2078f3d8c721b7e1e315d446a2c9586fe266b342
Polina Rogge <pr077@hdm-stuttgart.de>
2025-01-21 22:41:57 +0100

    Created Tag List -> not yet fully developed but Tags exist already

--- tschiraplus/UI/ViewModels/TaskCreationViewModel.cs
+++ tschiraplus/UI/ViewModels/TaskCreationViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Windows.Input;
 using CommunityToolkit.Mvvm.Input;
+using Services.DTOs;
 using Services.TaskServices;
 
 namespace UI.ViewModels;
@@ -20,21 +22,25 @@ public class TaskCreationViewModel : ViewModelBase
     public string Description {get; set;}
     
     public string InitialStatus {get; set;}
+   /* public Guid TagId {get; set;}
+    public string TagTitle {get; set;}
+    public string TagDescription {get; set;}
+    public string ColorCode {get; set;} */
     
     // Commands
     public ICommand CreateTaskCommand { get; }
     
     public ICommand ApplyTagCommand { get; }
-    
+
     public TaskCreationViewModel(ITaskService taskService, MainTabViewModel mainTabViewModel)
     {
         _taskService = taskService;
         _mainTabViewModel = mainTabViewModel;
-        CreateTaskCommand = new RelayCommand(CreateTask); 
-        ApplyTagCommand= new RelayCommand(ApplyTag);
+        CreateTaskCommand = new RelayCommand(CreateTask);
+        ApplyTagCommand = new RelayCommand(ApplyTag);
+
     }
-    
-    
+
     /// <summary>
     /// Uses the _taskService to create a new task
     /// </summary>
@@ -52,14 +58,35 @@ public class TaskCreationViewModel : ViewModelBase
 
     private void ApplyTag()
     {
+        /*
         var dto = _taskService.ApplyTagDto(
-            Title,
-            Description,
+            TagId,
+            TagTitle,
+            TagDescription,
             ColorCode );
-        _taskService.ApplyTag(dto);
-        
+        _taskService.ApplyTag(dto); */
+
+        var Documentation_Tag = new TagDto { TagId = Guid.NewGuid(), Title = "Documentation", ColorCode = "Blue" };
+        var Enhancement_Tag = new TagDto
+            { TagId = Guid.NewGuid(), Title = "Enhancement", Description = null, ColorCode = "violet" };
+        var Help_wanted_Tag = new TagDto
+            { TagId = Guid.NewGuid(), Title = "Help Wanted", Description = null, ColorCode = "orange" };
+        var Invalid_Tag = new TagDto
+            { TagId = Guid.NewGuid(), Title = "Invalid", Description = null, ColorCode = "yellow" };
+        var Needs_review_Tag = new TagDto
+            { TagId = Guid.NewGuid(), Title = "Needs Review", Description = null, ColorCode = "green" };
+        var Not_sprint_relevant_Tag = new TagDto
+            { TagId = Guid.NewGuid(), Title = "Not Sprint Relevant", Description = null, ColorCode = "turquoise" };
+        var Question_Tag = new TagDto
+            { TagId = Guid.NewGuid(), Title = "Question", Description = null, ColorCode = "pink" };
+        var Other_Tag = new TagDto { TagId = Guid.NewGuid(), Title = "Other", Description = null, ColorCode = "grey" };
+
     }
-    
-    public ObservableCollection<TaskCreationViewModel> SelectedTags { get; } = [];
-    public ObservableCollection<TaskCreationViewModel> AvailableTags { get; } = [];
+
+/*
+   public ObservableCollection<TaskCreationViewModel> SelectedTags { get; } = [];
+   public ObservableCollection<TaskCreationViewModel> AvailableTags { get; } = [
+       Documentation_Tag, Enhancement_Tag, Help_wanted_Tag, Invalid_Tag, Needs_review_Tag, Not_sprint_relevant_Tag, Question_Tag, Other_Tag];
+   */
 }
+
