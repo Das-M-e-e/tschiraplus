168731337a201653956119fcffed8c55886b69df
Anna-Lena <ma157@hdm-stuttgart.de>
2025-01-24 21:06:20 +0100

    Erstellen der Mapper-Methoden für UserModel und UserDtO

---
+++ tschiraplus/Services/Mapper/UserMapper.cs
@@ -0,0 +1,34 @@
+﻿using Core.Models;
+using Services.DTOs;
+
+namespace Services.Mapper;
+
+public class UserMapper
+{
+    public static UserDto toDto(UserModel model)
+    {
+        var ProjectDto = new UserDto()
+        {
+            UserId = model.UserId,
+            Username = model.Username
+        };
+        
+        return ProjectDto;
+    }
+
+
+    public static UserModel toModel(UserDto dto)
+    {
+        var UserModel = new UserModel()
+        {
+            UserId = dto.UserId,
+            Username = dto.Username,
+            
+        };
+        
+        return UserModel;
+    }
+    
+    
+    
+}
